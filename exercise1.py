0edacd1eac0b069f248a2ba042d6ea57bac7fe6a
Tyler Harangozo <tyler@harangozo.net>
2015-11-01 00:38:42 -0400

    added exercise1 and tests
    todo:
    - fill out pig_latinify() docstring
    - add more code comments if necessary
    - add more test cases if necessary

--- exercise1.py
+++ exercise1.py
@@ -23,4 +23,39 @@ def pig_latinify(word):
     """
     result = ""
 
+    vowels = ["a", "e", "i", "o", "u", "y"]
+
+    # sanitize input
+    if(type(word) is str):
+        word = word.strip()
+        try:
+            if (word.isalpha() is False):
+                return "Input error."
+            else:
+                word = word.lower()
+        except AttributeError:
+            return "Input error."
+    else:
+        return "Input error."
+
+    # get first letter type
+    if(word[0] in vowels):
+        first_type = "v"  # vowel condition
+        if(word[0] == "y"):
+            first_type = "c"  # treat y as consonant when beginning word
+            vowels.pop()  # remove y from the list of vowels
+    else:
+        first_type = "c"  # consonant condition
+
+    # convert word to pig latin
+    if(first_type == "v"):
+        result = word + "yay"
+    elif(first_type == "c"):
+        i = 0
+        word_len = len(word)
+        while word[i] not in vowels:
+            i += 1
+        slice_start = i
+        result = word[slice_start:word_len] + word[0:slice_start] + "ay"
+
     return result
--- test_exercise1.py
+++ test_exercise1.py
@@ -25,3 +25,18 @@ def test_basic():
     assert pig_latinify("apple") == "appleyay"
 
 
+def test_advanced():
+    assert pig_latinify('python') == 'ythonpay'
+    assert pig_latinify('yooper') == 'ooperyay'
+    assert pig_latinify('Youth') == 'outhyay'
+    assert pig_latinify(' spacey ') == 'aceyspay'
+    assert pig_latinify('supercallifragilisticexpialidocious'
+                        ) == 'upercallifragilisticexpialidocioussay'
+
+
+def test_bad_inputs():
+    assert pig_latinify('12345') == 'Input error.'
+    assert pig_latinify('two words') == 'Input error.'
+    assert pig_latinify('c0mb1n3dw0rd') == 'Input error.'
+    assert pig_latinify(12345) == 'Input error.'
+    assert pig_latinify(["list", 1]) == 'Input error.'
