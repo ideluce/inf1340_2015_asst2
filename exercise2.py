2a1f8ca349e4dda547a203e7999e746e1ceb4d3f
Tyler Harangozo <tyler@harangozo.net>
2015-11-01 01:08:55 -0500

    added exercise2
    todo:
    - test code with different inputs
    - add pytests
    - clean up where possible
    - add explanatory comments

--- exercise2.py
+++ exercise2.py
@@ -21,8 +21,32 @@ def find(input_string, substring, start, end):
     :raises:
 
     """
-    return -1
 
+    search_area = input_string[start:end]
+
+    i = 0
+    match_start = -1
+    in_match = False
+    while i < (len(search_area)-1):
+        letter = search_area[i]
+        if in_match is False:
+            if letter == substring[0]:
+                in_match = True
+                restart_i = i
+                j = 0
+                match_start = i + start
+                j += 1
+        elif in_match is True and j < len(substring):
+            if letter != substring[j]:
+                in_match = False
+                match_start = -1
+                i = restart_i
+            j += 1
+        elif in_match is True and j == len(substring):
+            return match_start
+        i += 1
+
+    return match_start
 
 def multi_find(input_string, substring, start, end):
     """
@@ -34,6 +58,13 @@ def multi_find(input_string, substring, start, end):
 
     """
     result = ""
-
+    i = start
+    j = i
+    while(i < end):
+        if (find(input_string, substring, j, end) != -1):
+            latestindex = find(input_string, substring, j, end)
+            result += str(latestindex)+","
+            j = latestindex+1
+        i += 1
+    result = result.rstrip(",")
     return result
-
